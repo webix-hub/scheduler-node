dfc305e53023c23ce1cbbbf16e5a8cc7841f3532
Helga <okustova@xbsoftware.com>
2021-04-09 16:55:32 +0300

    [fix] storing end of series outside of recurring pattern string

--- index.js
+++ index.js
@@ -31,29 +31,6 @@ function fixID(a){
 	return a;
 }
 
-// Retrieves end of repetition for recurring events
-function getEndRepeat(event) {
-	// FREQ=DAILY;INTERVAL=2;UNTIL=20211023T000000Z;
-	let endRepeat;
-
-	const parts = event.recurring.split(";");
-	for (let i = 0; i < parts.length; i++) {
-		const [key, value] = parts[i].split("=");
-		if (key === "UNTIL") endRepeat = strToDate(value);
-	}
-
-	return endRepeat;
-}
-
-// Converts date strings (20211023T000000Z) to Date objects
-function strToDate(str) {
-	return new Date(
-		str.substr(0, 4) * 1,
-		str.substr(4, 2) * 1 - 1,
-		str.substr(6, 2) * 1
-	);
-}
-
 app.get("/events", (req, res, next) => {
 	const from = req.query.from;
 	const to = req.query.to;
@@ -63,20 +40,18 @@ app.get("/events", (req, res, next) => {
 			start_date:{ $lt: req.query.to },
 			$or:[
 				{ end_date:{ $gte: req.query.from } },
-				{ $not: { recurring: "" } }
+				{ series_end_date:{ $gte: req.query.from } },
+				{ 
+					$and:[
+						{ $not: { recurring: "" } },
+						{ series_end_date: "" }
+					]
+				}
 			]
 		}).sort({ start_date: 1 }).exec((err, data) => {
 			if (err)
 			    next(err);
 			else {
-				const fromDate = new Date(from);
-				data = data.filter(event => {
-					if (event.recurring) {
-						const end = getEndRepeat(event);
-						return !end || end > fromDate;
-					} else return true;
-				});
-
 				res.send(data.map(fixID));
 			}
 		});
@@ -99,7 +74,8 @@ const allowedFields = [
 	"color",
 	"recurring",
 	"calendar",
-	"origin_id"
+	"origin_id",
+	"series_end_date"
 ];
 
 app.put("/events/:id", (req, res, next) => {
